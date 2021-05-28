815d1afc881be0d41167700cece325edf34826be
yogesh0310 <yogeshg7770@gmail.com>
2021-05-28 23:10:23 +0530

    Third

---
+++ C++/Array/RearrangeNegativeAndPositive.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[]={-12, 11, -13, -5, 6, -7, 5, -3, -6};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]<0)
+		{
+			
+		}
+	}
+}
